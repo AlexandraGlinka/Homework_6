f15deac0813550bfad583c62cd6321e86bfbb58d
glinka <alexandraglinka@yandex.ru>
2023-02-28 21:06:16 +0300

    Task_2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-        //task2();
+        task2();
     }
     public static void task1 () {
         System.out.println("Задача 1");
@@ -9,6 +9,13 @@ public class Main {
         for (int i = 1; i <= 10; i++){
             System.out.println(i);
         }
+    }
 
+    public static void task2() {
+        System.out.println("Задача 2");
+
+        for (int k = 10; k > 0; k -= 1) {
+            System.out.println(k);
+        }
     }
 }
