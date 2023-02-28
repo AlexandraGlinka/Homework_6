fa67641efa917e355015ac63c7ec6d34453a36d7
glinka <alexandraglinka@yandex.ru>
2023-02-28 21:10:30 +0300

    Task_3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
     public static void task1 () {
         System.out.println("Задача 1");
@@ -16,6 +17,17 @@ public class Main {
 
         for (int k = 10; k > 0; k -= 1) {
             System.out.println(k);
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        // Выведите в консоль все четные числа от 0 до 17.
+
+        for (int n = 0; n < 17; n++) {
+            if (n % 2 == 0 && n != 0) {
+                System.out.println(n);
+            }
         }
     }
 }
